4804ea5821948a75a50cf31fd113850c39960a00
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-04-25 13:17:07 +0300

    deployed

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -46,7 +46,7 @@ const Customizer = () => {
     try {
       setgeneratingImg(true);
 
-      const response = await fetch('http://localhost:5000/', {
+      const response = await fetch('https://dalle-server-n2wj.onrender.com/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
